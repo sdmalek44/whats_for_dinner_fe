16d1fda91173af81f16a7a3749d377b7e8d574a8
brickstar <brickstar@gmail.com>
2018-12-27 22:49:17 -0700

    Initial

---
+++ index.js
@@ -0,0 +1,17 @@
+const loginUser = () => {
+  var email = document.getElementById("email").value
+  var password = document.getElementById("password").value
+  console.log(email)
+  console.log(password)
+  fetch(`https://api-whats-for-dinner.herokuapp.com/api/v1/login`, {
+    method: "post",
+    headers: {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({user: {email: email, password: password} })
+  }).then(res => res.json())
+    .then(res => sessionStorage.setItem("user_token", res.token));
+}
+
+document.getElementById("login").addEventListener("click", loginUser)
