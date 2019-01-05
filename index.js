446410c0f90a7ac3e4f0af657dc661ecf9c3bee4
stephen <malek.stephen44@gmail.com>
2019-01-05 13:51:02 -0700

    Add handling if incorrect login or field empty

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const loginUser = () => {
   var email = document.getElementById("email").value
   var password = document.getElementById("password").value
-  fetch(`https://api-whats-for-dinner.herokuapp.com/api/v1/login`, {
-    method: "post",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({user: {email: email, password: password} })
-  }).then(res => res.json())
-    .then(res => sessionStorage.setItem("user_token", res.token));
+  if (password !== "" && email !== ""){
+    fetch(`https://api-whats-for-dinner.herokuapp.com/api/v1/login`, {
+      method: "post",
+      headers: {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({user: {email: email, password: password} })
+    })
+    .then(res => res.json())
+    .then(res => {
+      if (res.token){
+        sessionStorage.setItem("user_token", res.token);
+        document.location = "search.html"
+      } else {
+        document.getElementById('flash').innerHTML = "Incorrect Username or Password"
+      }
+    })
+  } else {
+    document.getElementById('flash').innerHTML = "Please Enter Email and Password"
+  }
 }
 
 document.getElementById("login").addEventListener("click", loginUser)
