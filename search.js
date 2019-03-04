a5ea0abb9f280fb7ea530ee8a9d8c685ea9304e4
stephen <malek.stephen44@gmail.com>
2019-03-03 22:15:19 -0700

    Add dropdown checkbox for allergies and handle search button WIP

--- search.js
+++ search.js
@@ -1,3 +1,15 @@
+var expanded = false;
+
+function showCheckboxes() {
+  var checkboxes = document.getElementById("checkboxes");
+  if (!expanded) {
+    checkboxes.style.display = "block";
+    expanded = true;
+  } else {
+    checkboxes.style.display = "none";
+    expanded = false;
+  }
+}
 
 const logOut = () => {
   if(sessionStorage.user_token){
@@ -7,7 +19,10 @@ const logOut = () => {
 }
 
 const searchRecipes = () => {
-  
+  var allergies = [];
+  var checkboxes = document.getElementsByClassName('cb');
+  Array.prototype.forEach.call(checkboxes, function(cb){ if (cb.checked){ allergies.push(cb.value) } });
+  console.log(allergies)
 }
 
 document.getElementById('logout').addEventListener('click', logOut)
